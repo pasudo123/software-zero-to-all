c5c08aac80f3b4e1259dbc01e41a326463af3211
PARK SUNG DONG <oraedoa@gmail.com>
2020-10-24 16:52:03 +0900

    leetcode

--- leetcode/203. Remove Linked List Elements.java
+++ leetcode/203. Remove Linked List Elements.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// solution 보고 푼 것.
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         
@@ -27,10 +29,40 @@ class Solution {
             if (post.val == val) {  
                 pre.next = post.next;
             } else {  
+                // post 가 이전 단계
                 pre = post;
             }
             
+            // post 를 다음단계로 보낸다.
+            // pre 랑 post 랑 한 단계 차이를 만든다.
             post = post.next; 
+        }
+    
+        return head;
+    }
+}
+
+// solution 보지 않고 직접 푼 것.
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        while(head != null && head.val == val) {
+            head = head.next;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode post = head;
+        
+        while(post != null) {
+            if(post.val == val) {
+                pre.next = post.next;
+            } else {
+                pre = pre.next;
+            }
+            
+            post = post.next;
         }
     
         return head;
