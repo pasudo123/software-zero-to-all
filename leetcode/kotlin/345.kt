eebbcd4640e8a21ee4816796c2efc3c60bf946a3
PARK SUNG DONG <pasudo123@naver.com>
2024-10-30 22:49:05 +0900

    Update 345.kt

--- leetcode/kotlin/345.kt
+++ leetcode/kotlin/345.kt
@@ -21,3 +21,31 @@ class Solution {
         return result
     }
 }
+
+class Solution {
+    fun reverseVowels(s: String): String {
+        val vowels = setOf("a", "A", "e", "E", "i", "I", "o", "O", "u", "U")
+        val charArray = s.toCharArray()
+        var start = 0
+        var end = charArray.size - 1
+
+        while (start < end) {
+
+            while (start < end && (charArray[start].toString() in vowels).not()) {
+                start++
+            }
+            while (start < end && (charArray[end].toString() in vowels).not()) {
+                end--
+            }
+
+            val temp = charArray[start]
+            charArray[start] = charArray[end]
+            charArray[end] = temp
+
+            start++
+            end--
+        }
+
+        return String(charArray)
+    }
+}
