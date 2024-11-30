2c58fc98f6813f3a8d824fb870f92ef7925008dd
PARK SUNG DONG <pasudo123@naver.com>
2024-11-30 10:40:53 +0900

    Create 452.kt

---
+++ leetcode/kotlin/452.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun findMinArrowShots(points: Array<IntArray>): Int {
+        points.sortBy { it[1] }
+
+        var count = 0
+        var prevEnd = Int.MIN_VALUE
+
+        for (point in points) {
+            val (start, end) = point
+            
+            // 이전 풍선 구간에 겹치지 않는 곳에 화살을 발사. count++
+            // 그리고 구간을 새롭게 갱신한다
+            if (start > prevEnd) {
+                count++
+                prevEnd = end
+            }
+        }
+
+        return count
+    }
+}
